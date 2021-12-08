b37ca0f62eb6e611c906e661f1433137213f7a10
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-12-08 09:05:15 +0700

    feat(BookCard): add skeleton loading

--- src/components/BookCard/styles.js
+++ src/components/BookCard/styles.js
@@ -220,3 +220,226 @@ export const BookCardWrapper = styled.article`
     }
   }
 `;
+
+export const BookCardSkeletonWrapper = styled.div`
+  /* Mobile */
+  box-sizing: content-box;
+  width: 100%;
+  min-height: 120px;
+  border: 3px solid lightgray;
+  border-radius: 15px;
+  display: grid;
+  grid-template-columns: ${(props) => (!props.isQuote ? "80px" : "")} 1fr 60px;
+  .card-img {
+    padding: 10px 0 10px 10px;
+    .img-placeholder {
+      background-color: lightgray;
+      height: 100px;
+      width: 65px;
+      border-radius: 7px;
+    }
+  }
+  .card-desc {
+    padding: 10px;
+    .card-status {
+      font-size: 11px;
+      font-family: Inter, Arial, Helvetica, sans-serif;
+      color: ${colorPalette.secondary.rgb()};
+      margin-bottom: 5px;
+      span {
+        width: 20%;
+        display: inline-block;
+        height: 12px;
+        background: lightgray;
+        border-radius: 10px;
+      }
+    }
+    .book-title {
+      font-size: 16px;
+      font-weight: bold;
+      font-family: Raleway, Arial, Helvetica, sans-serif;
+      margin-bottom: 5px;
+      span {
+        width: 100%;
+        display: inline-block;
+        height: 16px;
+        background: lightgray;
+        border-radius: 10px;
+      }
+    }
+    .book-author {
+      font-size: 11px;
+      font-family: Inter, Arial, Helvetica, sans-serif;
+      color: ${colorPalette.secondary.rgb()};
+      margin-bottom: 5px;
+      span.book-author-placeholder {
+        width: 10%;
+        display: inline-block;
+        height: 11px;
+        background: lightgray;
+        border-radius: 10px;
+      }
+      span {
+        color: black;
+      }
+    }
+    .book-year {
+      font-size: 11px;
+      font-family: Inter, Arial, Helvetica, sans-serif;
+      margin-bottom: 5px;
+      span {
+        width: 50px;
+        display: inline-block;
+        height: 12px;
+        background: lightgray;
+        border-radius: 10px;
+      }
+    }
+    .rating {
+      margin-top: 4px;
+      margin-bottom: 7px;
+    }
+    .review-body {
+      font-size: 11px;
+      font-family: Inter, Arial, Helvetica, sans-serif;
+    }
+  }
+  .card-menu {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: end;
+    .menu-button,
+    .action-button,
+    .share-button {
+      width: 30px;
+      height: 30px;
+      background-color: lightgray;
+      border-radius: 100%;
+      font-size: 12px;
+      font-weight: bold;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 20px;
+        height: 20px;
+      }
+      &:active {
+        filter: brightness(0.75);
+      }
+    }
+  }
+
+  /* Desktop */
+  @media (min-width: 768px) {
+    max-width: 765px;
+    min-height: 180px;
+    border-radius: 30px;
+    grid-template-columns: ${(props) => (!props.isQuote ? "115px" : "")} 1fr 60px;
+    .card-img {
+      padding: 15px 0 15px 15px;
+      .img-placeholder {
+        height: 150px;
+        width: 100px;
+        border-radius: 20px;
+      }
+    }
+    .card-quote {
+      padding: 20px;
+      .card-status {
+        font-size: 12px;
+        font-family: Inter, Arial, Helvetica, sans-serif;
+        color: ${colorPalette.secondary.rgb()};
+        margin-bottom: 5px;
+      }
+      .quote-body {
+        font-size: 24px;
+        font-weight: bold;
+        font-family: Raleway, Arial, Helvetica, sans-serif;
+        margin-bottom: 5px;
+      }
+      .quoted-from {
+        font-size: 14px;
+        font-family: Inter, Arial, Helvetica, sans-serif;
+        color: ${colorPalette.secondary.rgb()};
+        margin-bottom: 5px;
+        span {
+          color: black;
+        }
+      }
+    }
+    .card-desc {
+      padding: 15px;
+      .card-status {
+        font-size: 12px;
+        font-family: Inter, Arial, Helvetica, sans-serif;
+        color: ${colorPalette.secondary.rgb()};
+        margin-bottom: 5px;
+      }
+      .book-title {
+        text-decoration: none;
+        color: inherit;
+        font-size: 36px;
+        font-weight: bold;
+        font-family: Raleway, Arial, Helvetica, sans-serif;
+        margin-bottom: 5px;
+        &:hover {
+          text-decoration: underline;
+        }
+        span {
+          width: 100%;
+          height: 24px;
+          display: inline-block;
+          background: lightgray;
+          border-radius: 36px;
+        }
+      }
+      .book-author {
+        font-size: 16px;
+        font-family: Inter, Arial, Helvetica, sans-serif;
+        color: ${colorPalette.secondary.rgb()};
+        margin-bottom: 5px;
+        span {
+          color: black;
+        }
+        span.book-author-placeholder {
+          height: 16px;
+          display: inline-block;
+          background: lightgray;
+          border-radius: 10px;
+        }
+      }
+      .book-year {
+        font-size: 16px;
+        font-family: Inter, Arial, Helvetica, sans-serif;
+        margin-bottom: 5px;
+        span {
+          height: 16px;
+          display: inline-block;
+          background: lightgray;
+          border-radius: 10px;
+        }
+      }
+      .rating {
+        margin-top: 4px;
+        margin-bottom: 7px;
+      }
+      .review-body {
+        font-size: 16px;
+        font-family: Inter, Arial, Helvetica, sans-serif;
+      }
+    }
+    .card-menu {
+      padding: 15px 15px 15px 0;
+      .menu-button,
+      .action-button,
+      .share-button {
+        width: 40px;
+        height: 40px;
+      }
+    }
+  }
+`;
--- src/modules/FeedModule.js
+++ src/modules/FeedModule.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import BookCard from "../components/BookCard";
+import { BookCardSkeleton } from "../components/BookCard/BookCard";
 
 export function FeedModule(props) {
   const [feed, setFeed] = useState([]);
+  const [feedLoading, setFeedLoading] = useState(false);
 
   async function getFeed() {
     try {
+      setFeedLoading(true);
       const response = await axios.get(`${process.env.REACT_APP_MOCK_API_URL}/feed`);
       console.log(response.data);
       setFeed(response.data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setFeedLoading(false);
     }
   }
 
@@ -19,7 +24,14 @@ export function FeedModule(props) {
     getFeed();
   }, []);
 
-  return (
+  return feedLoading ? (
+    <>
+      <BookCardSkeleton />
+      <BookCardSkeleton />
+      <BookCardSkeleton />
+      <BookCardSkeleton />
+    </>
+  ) : (
     <>
       {feed.map((item) => (
         <BookCard key={item.activity_id} {...item} />
